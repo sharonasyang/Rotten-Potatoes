01bbd2bb9e3e4563bc4119044ae90931eaf461df
sharonasyang <52472053+sharonasyang@users.noreply.github.com>
2022-01-13 19:05:55 -0800

    Create about.js

---
+++ about.js
@@ -0,0 +1,13 @@
+function openHome() {
+  document.location.href = "file:///C:/Users/gwc/Desktop/Python%20Project/home.html";
+}
+function openMovies() {
+  document.location.href = "file:///C:/Users/gwc/Desktop/Python%20Project/home.html#page-Movies";
+}
+// BOTH GO HOME
+function openReviews() {
+  document.location.href = "file:///C:/Users/gwc/Desktop/Python%20Project/reviews.html";
+}
+function openAbout() {
+  document.location.href = "file:///C:/Users/gwc/Desktop/Python%20Project/about.html";
+}
